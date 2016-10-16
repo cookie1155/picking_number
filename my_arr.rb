80f45b3882cc00f422dfbaa224e7bc3edfa443bd
ak <cookie115577@gmail.com>
2016-10-16 13:13:50 +0900

    MyArr#missing_numberを計算量を考慮したバージョンに改修

--- my_arr.rb
+++ my_arr.rb
@@ -6,15 +6,35 @@ class MyArr
     @arr = arr
   end
 
-  def missing_number
-    ret = nil
-    arr.sort.each_with_index do |number, i|
-      next if number == i + 1
+  def sorted_arr
+    return @sorted_arr if @sorted_arr
 
-      ret = i + 1
-      break
+    sorted_arr = arr.sort
+    # 最後尾の数字も探索できるように、最後に数字を追加する
+    sorted_arr << sorted_arr.size + 2
+    @sorted_arr = sorted_arr
+  end
+
+  def missing_number
+    left = 0
+    right = sorted_arr.size - 1
+    mid = nil
+
+    # leftとrightが同じ値に収束するまで二分探索を行う
+    # この収束した値が、取り出された数字が入っていたはずのindexになる
+    while left < right do
+      mid = (left + right) / 2
+
+      # 取り出された数字がmidより後だった場合
+      if sorted_arr[mid] == mid + 1
+        left = mid + 1
+
+      # 取り出された数字がmidまたはmidより前だった場合
+      else
+        right = mid
+      end
     end
 
-    ret
+    left + 1
   end
 end
