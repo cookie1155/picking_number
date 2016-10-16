7e1aacd9a6bb8159a031889a3b778ec00505fd80
ak <cookie115577@gmail.com>
2016-10-16 13:14:14 +0900

    極限値のテスト追加

--- spec/my_arr_spec.rb
+++ spec/my_arr_spec.rb
@@ -3,10 +3,12 @@ require_relative '../my_arr'
 
 RSpec.describe MyArr, '#missing_number' do
   it '取り除かれた数字が返却される' do
-    arr = (1..1000).to_a.shuffle
-    arr.delete(247)
-    my_arr = MyArr.new(arr)
+    [1, 247, 1000].each do |i|
+      arr = (1..1000).to_a.shuffle
+      arr.delete(i)
+      my_arr = MyArr.new(arr)
 
-    expect(my_arr.missing_number).to eq 247
+      expect(my_arr.missing_number).to eq i
+    end
   end
 end
